015afef2c4eb73d0749d4aa5a93cbc206dd2b5fe
patdie421 <patrice.dietsch@gmail.com>
2020-05-19 23:30:26 +0200

    Update ds18b20_add.h

--- main/ds18b20_add.h
+++ main/ds18b20_add.h
@@ -6,4 +6,6 @@
 #define DS18B20_RESOLUTION_11_BIT  0x5F
 #define DS18B20_RESOLUTION_12_BIT  0x7F
 
+esp_err_t ds18b20_set_resolution(gpio_num_t pin, ds18x20_addr_t addr, uint8_t resolution);
+
 #endif
