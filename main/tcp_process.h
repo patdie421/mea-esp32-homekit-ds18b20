7bdc71e6c5b59d6409ac57b6457dc5b2f00fd87e
patdie421 <patrice.dietsch@gmail.com>
2020-08-03 13:26:02 +0200

    Update tcp_process.h

--- main/tcp_process.h
+++ main/tcp_process.h
@@ -3,6 +3,6 @@
 
 #include "config.h"
 
-void tcp_process(int sock, struct mea_config_s *mea_config, int8_t mode, char cmd, char *parameters, void *userdata);
+int tcp_process(int sock, struct mea_config_s *mea_config, int8_t mode, char cmd, char *parameters, void *userdata);
 
 #endif
