d6c7ca6368042b3bd32cb37767acc750ee616c64
patdie421 <patrice.dietsch@gmail.com>
2020-08-03 13:34:48 +0200

    Update tcp_server.h

--- main/tcp_server.h
+++ main/tcp_server.h
@@ -6,6 +6,12 @@
 #define TCP_SERVER_RESTRICTED 0
 #define TCP_SERVER_CONFIG 1
 
+#define OK_STATUS "OK"
+#define KO_STATUS "KO"
+#define BAD_COMMAND_STATUS "???"
+#define BAD_REQUEST_STATUS "!!!"
+#define BAD_CREDENCIAL "###"
+
 typedef int (*tcp_process_callback_t)(int sock, struct mea_config_s *mea_config, int8_t mode, char cmd, char *parameters, void *userdata);
 
 void tcp_send_data(const int sock, char *data);
